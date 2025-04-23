7401d2a318f4fc68bf68fc2cbb0f9bb5abf178dd
muhannadpv786 <p.vmuhannad786@gmail.com>
2025-04-23 20:48:23 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -14,7 +14,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "muhannadthreetier"
+    bucket = "muhannadthreetier1"
     key    = "terraform.tfstate"
     region = "ap-south-1"
     //  dynamodb_table = "my-terraform-infra-table"
