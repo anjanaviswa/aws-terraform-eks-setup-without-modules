05c477000f54fda0f6c805e52154d1468eb42841
anjanaviswa <167313335+anjanaviswa@users.noreply.github.com>
2025-04-27 16:08:05 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -14,7 +14,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "anjanabucket"
+    bucket = "anjanabucket12345"
     key    = "terraform.tfstate"
     region = "ap-south-1"
     //  dynamodb_table = "my-terraform-infra-table"
